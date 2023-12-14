04607841a96f7cb3d930d0b1caa8f1f4b2632c20
geonwoo0215 <gw980215@naver.com>
2023-12-15 02:00:13 +0900

    feat: 무한 스크롤 추가

--- js/partys.js
+++ js/partys.js
@@ -1,50 +1,79 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    let page = 0;
+    const size = 10;
     const token = localStorage.getItem("token");
-
-    if(!token) {
-        window.location.href = "../html/login.html";
+  
+    if (!token) {
+      window.location.href = "../html/login.html";
     } else {
-        fetch("http://localhost:8080/partys", {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-            },
+      function infiniteScroll() {
+  
+        fetch(`http://localhost:8080/partys?page=${page}&size=${size}`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${token}`,
+          },
         })
         .then(response => {
-            if(!response.ok) {
-                throw new Error("Failed")
-            }
-
-            return response.json();
+          if (!response.ok) {
+            throw new Error("Failed");
+          }
+  
+          return response.json();
         })
         .then(data => {
-            renderPartyPage(data.data);
+          renderPartyPage(data.data);
+          page++;
         })
         .catch(error => {
-            console.error("Error during party data fetch:", error);
+          console.error("Error during party data fetch:", error);
         });
+      }
+  
+      function renderPartyPage(partyDataList) {
+        console.log("Party Data List:", partyDataList);
+        const appContainer = document.getElementById("app");
+  
+        partyDataList.forEach(partyData => {
+          const partyElement = createPartyElement(partyData);
+          appContainer.appendChild(partyElement);
+        });
+      }
+  
+      function createPartyElement(partyData) {
+        const partyElement = document.createElement("div");
+        partyElement.classList.add("party");
+  
+        partyElement.innerHTML = `
+            <h2>${partyData.name}</h2>
+            <p>${partyData.content}</p>
+            <p>모임 날짜: ${partyData.startDate}</p>
+        `;
+  
+        return partyElement;
+      }
+  
+      infiniteScroll();
+
+      window.addEventListener('scroll', debounce(function () {
+        if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+          infiniteScroll();
+        }
+      }, 300));
     }
-});
+  
 
-function renderPartyPage(partyDataList) {
-    console.log("Party Data List:", partyDataList);
-    const appContainer = document.getElementById("app");
-
-    partyDataList.forEach(partyData => {
-        const partyElement = createPartyElement(partyData);
-        appContainer.appendChild(partyElement);
-    });
-}
-
-function createPartyElement(partyData) {
-    const partyElement = document.createElement("div");
-    partyElement.classList.add("party");
-
-    partyElement.innerHTML = `
-        <h2>${partyData.name}</h2>
-        <p>${partyData.content}</p>
-        <p>Start Date: ${partyData.startDate}</p>
-    `;
-
-    return partyElement;
-}
+    function debounce(func, wait) {
+      let timeout;
+      return function () {
+        const context = this,
+          args = arguments;
+        const later = function () {
+          timeout = null;
+          func.apply(context, args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+      };
+    }
+  });
